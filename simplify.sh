f820eab5b7413a40cb417916953255047511d2d4
Dan Phiffer <dan@phiffer.org>
2018-03-21 19:27:17 -0400

    :barber: simplified FeatureCollection => Feature

--- simplify.sh
+++ simplify.sh
@@ -2,6 +2,18 @@
 
 for file in data/*/*.geojson; do \
 	simple=`echo $file | sed -e "s/\.geojson/.dp20.geojson/"` ; \
-	echo $simple ; \
-	mapshaper $file -simplify dp 20% -o format=geojson $simple ; \
+	if [[ $simple = *".dp20.dp20"* ]] ; then \
+		if [ -f "$simple" ] ; then
+			echo "Removing $simple" ; \
+			rm $simple ; \
+		fi \
+	else \
+		echo $simple ; \
+		if [ ! -f "$simple" ] ; then \
+			mapshaper $file \
+				-simplify dp 20% \
+				-o format=geojson geojson-type=Feature \
+				$simple ; \
+		fi \
+	fi \
 done
