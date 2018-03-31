806a9b925944fbb445f3fd6c8c8badde55143850
Dan Phiffer <dan@phiffer.org>
2018-03-30 22:08:23 -0400

    :bug: fix shell paths

--- scripts/simplify.sh
+++ scripts/simplify.sh
@@ -4,8 +4,8 @@ WHOAMI=`python -c 'import os, sys; print os.path.realpath(sys.argv[1])' $0`
 SCRIPTS=`dirname $WHOAMI`
 ROOT=`dirname $SCRIPTS`
 
-for file in "$ROOT/data/*/*.lookup.geojson"; do \
-	simple=`echo $file | sed -e "s/\.lookup.geojson/.display.geojson/"` ; \
+for file in $ROOT/data/*/*.lookup.geojson ; do \
+	simple=`echo $file | sed -e "s/\.lookup\.geojson/.display.geojson/"` ; \
 	echo $simple ; \
 	if [ ! -f "$simple" ] ; then \
 		mapshaper $file \
