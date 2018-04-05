2d9421d5a1dd47305e05a81a5872aedfe4dd0bfd
Dan Phiffer <dan@phiffer.org>
2018-04-05 07:33:14 -0400

    :key: run with 'app' user

--- server/start.sh
+++ server/start.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-su - postgres -c '/usr/lib/postgresql/10/bin/pg_ctl -D /app/pgdata start'
+/usr/lib/postgresql/10/bin/pg_ctl -D /app/pgdata start
 gunicorn -c gunicorn.py server:app
