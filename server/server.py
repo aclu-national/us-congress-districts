46e1d225a0b8b4fe1a730f40137dba1e7b121f8c
Dan Phiffer <dan@phiffer.org>
2018-04-09 13:16:43 -0400

    :zap: new endpoints: /districts and /sessions

--- server/server.py
+++ server/server.py
@@ -8,15 +8,35 @@ flask_cors.CORS(app)
 @app.before_request
 def init():
 	db_connect()
+	setup_sessions()
 
 def db_connect():
 	default_dsn = "user=postgres password=%s" % os.getenv('POSTGRES_PASSWORD')
 	db_dsn = os.getenv('POSTGRES_DSN', default_dsn)
 	flask.g.db = psycopg2.connect(db_dsn)
 
+def setup_sessions():
+	flask.g.sessions = {}
+	cur = flask.g.db.cursor()
+	cur.execute('''
+		SELECT id, start_date, end_date
+		FROM sessions
+		ORDER BY id DESC
+	''')
+
+	rs = cur.fetchall()
+	results = []
+	if rs:
+		for row in rs:
+			id = row[0]
+			flask.g.sessions[id] = {
+				"start_date": str(row[1]),
+				"end_date": str(row[2])
+			}
+
 @app.route("/")
 def hello():
-	return "Hello, you probably want to use: /pip"
+	return "Hello, you probably want to use: /pip, /districts, or /sessions"
 
 @app.route("/pip")
 def pip():
@@ -25,11 +45,17 @@ def pip():
 	lng = flask.request.args.get('lng', None)
 
 	if lat == None or lng == None:
-		return "Please include lat and lng args."
+		return "Please include 'lat' and 'lng' args."
+
+	if not re.match('^-?\d+(\.\d+)?', lat):
+		return "Please include a numeric 'lat'."
+
+	if not re.match('^-?\d+(\.\d+)?', lng):
+		return "Please include a numeric 'lng'."
 
 	cur = flask.g.db.cursor()
 	cur.execute('''
-		SELECT id, start_session, end_session, district_num, boundary_simple
+		SELECT id, name, start_session, end_session, district_num, boundary_simple
 		FROM districts
 		WHERE ST_within(ST_GeomFromText('POINT({lng} {lat})', 4326), boundary_geom)
 		ORDER BY end_session DESC
@@ -39,12 +65,17 @@ def pip():
 	results = []
 	if rs:
 		for row in rs:
+			start_session = row[2]
+			end_session = row[3]
 			results.append({
 				'id': row[0],
-				'start_session': row[1],
-				'end_session': row[2],
-				'district_num': row[3],
-				'boundary_simple': row[4]
+				'name': row[1],
+				'start_session': start_session,
+				'end_session': end_session,
+				'start_date': flask.g.sessions[start_session]['start_date'],
+				'end_date': flask.g.sessions[end_session]['end_date'],
+				'district_num': row[4],
+				'boundary_simple': row[5]
 			})
 
 	cur.close()
@@ -52,6 +83,54 @@ def pip():
 	rsp = {
 		'ok': 1,
 		'results': results
+	}
+	return flask.jsonify(rsp)
+
+@app.route("/districts")
+def districts():
+
+	ids = flask.request.args.get('ids', None)
+
+	if ids == None or not re.match('^\d+(,\d+)*$', ids):
+		return "Please include 'ids' arg (comma-separated numeric IDs)."
+
+	cur = flask.g.db.cursor()
+	cur.execute('''
+		SELECT id, name, start_session, end_session, district_num, boundary_simple
+		FROM districts
+		WHERE id IN ({ids})
+		ORDER BY end_session DESC
+	'''.format(ids=ids))
+
+	rs = cur.fetchall()
+	results = []
+	if rs:
+		for row in rs:
+			start_session = row[2]
+			end_session = row[3]
+			results.append({
+				'id': row[0],
+				'start_session': start_session,
+				'end_session': end_session,
+				'start_date': flask.g.sessions[start_session]['start_date'],
+				'end_date': flask.g.sessions[end_session]['end_date'],
+				'district_num': row[4],
+				'boundary_simple': row[5]
+			})
+
+	cur.close()
+
+	rsp = {
+		'ok': 1,
+		'results': results
+	}
+	return flask.jsonify(rsp)
+
+@app.route("/sessions")
+def sessions():
+	rsp = {
+		'ok': 1,
+		'results': flask.g.sessions
 	}
 	return flask.jsonify(rsp)
 
