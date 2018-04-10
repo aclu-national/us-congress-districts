c5ce8293de303cbe196802c5caca1d665723bff2
Dan Phiffer <dan@phiffer.org>
2018-04-10 17:44:16 -0400

    :wrench: tweaks to setup process

--- scripts/setup_ubuntu.sh
+++ scripts/setup_ubuntu.sh
@@ -15,13 +15,17 @@ apt upgrade -y
 apt install -y fail2ban ufw htop emacs24-nox postgresql postgresql-contrib \
                build-essential gdal-bin python python-pip python-gevent
 
+echo ""
+echo "Configuring ufw firewall, you will need to confirm this"
+echo "---------------------------------------------------------"
 ufw allow 5000
 ufw allow 22
 ufw enable
 
+echo ""
 echo "Adding Ubuntu GIS apt repo, you will need to confirm this"
 echo "---------------------------------------------------------"
-add-apt-repository ppa:ubuntugis/ubuntugis-unstable
+add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
 
 apt update
 apt install -y postgis
@@ -33,9 +37,9 @@ if [ -f "$POSTGRES_MAIN/postgresql.conf" ] ; then
 	mv "$POSTGRES_MAIN/postgresql.conf" "$POSTGRES_MAIN/postgresql.conf.bak"
 fi
 
-ln -s "$PROJECT/server/postgresql.conf" "$POSTGRES_MAIN/postgresql.conf"
+cp "$PROJECT/server/postgresql.conf" "$POSTGRES_MAIN/postgresql.conf"
 service postgresql restart
 
 sudo -u postgres createuser --superuser ubuntu
 sudo -u ubuntu createdb us_congress
-sudo -u ubuntu psql -c "CREATE EXTENSION postgis;"
+sudo -u ubuntu psql -d us_congress -c "CREATE EXTENSION postgis;"
